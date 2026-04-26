8ccf87f221228b51616abfd3bf823161243ac907
IrviiingVZ <150636449+IrviiingVZ@users.noreply.github.com>
2026-04-26 13:46:28 -0600

    Add files via upload

---
+++ EVA_10_INTERFACES_HERENCIA/src/main/java/com/mycompany/eva_10_interfaces_herencia/EVA_10_INTERFACES_HERENCIA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_10_interfaces_herencia;
+
+/**
+ *
+ * @author irvingvalles
+ */
+public class EVA_10_INTERFACES_HERENCIA {
+
+    public static void main(String[] args) {
+        Empleado empleado=new Empleado("Irving", "Yael", 23, "Jefe", 40000);
+        empleado.mostrarMensaje();
+    }
+}
